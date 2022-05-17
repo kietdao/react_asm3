558545584e5cfbd9902223b1faf1e61830226910
kietdao <43434606+kietdao@users.noreply.github.com>
2022-05-17 14:58:53 +0700

    first upload react_asm3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
+  const [userName, setUserName] = useState('')
+  const [data,setData] = useState(null)
+
+  useEffect(() => {
+    async function getUserInfo() {
+      try {
+        const response = await getUserData(userName)
+        console.log(response)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    getUserInfo()
+  }, [userName])
+
+  const handleInputUsername = (e) => {
+    setUserName(e.target.value)
+  }
+
+  const getUserData = (inputUserName) => {
+    return axios.get(`https://api.github.com/users${inputUserName ? `/${inputUserName}` : ''}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search-user'>
+        <span>Name: </span>
+        <input type='text' placeholder='Input name...' onChange={handleInputUsername}></input>
+      </div>
+
+      <div className="list-user">
+        <h2>User Detail Information</h2>
+        <div className='user-item'>
+          <div className='user-avatar'>
+            <img src=''/>
+          </div>
+          <div className='user-info'>
+            <div className='user-info-item'>
+              <p>Role: </p>
+              <p>Email: </p>
+            </div>
+            <div className='user-info-item'>
+              <p>Company: </p>
+            </div>
+            <div className='user-info-item'>
+              <p>Number of followers: </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
